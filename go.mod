b90b9f85c56a488b3b35488079d7f82eb2f1b443
nitpum <nitpum@outlook.com>
2022-01-03 19:40:30 +0700

    feat: Auto add role to bot

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/nitpum/inaho-yamato
 
 go 1.17
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
+
+require (
+	github.com/gorilla/websocket v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+)
---
+++ main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"gopkg.in/yaml.v3"
+)
+
+var (
+	Token          string
+	ConfigFilepath string
+	config         configData
+)
+
+type configData struct {
+	BotRole struct {
+		Enabled bool     `yaml:"enabled"`
+		Roles   []string `yaml:"roles"`
+	} `yaml:"bot_role"`
+	Nickname struct {
+		Enabled bool `yaml:"enabled"`
+		Members []struct {
+			ID     string   `yaml:"id"`
+			Prefix []string `yaml:"prefix"`
+		} `yaml:"members"`
+	} `yaml:"nickname"`
+}
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.StringVar(&ConfigFilepath, "c", "config.yaml", "Config path")
+	flag.Parse()
+}
+
+func main() {
+
+	if Token == "" {
+		panic("Token is required")
+	}
+
+	c, err := readConfig(ConfigFilepath)
+	if err != nil {
+		panic(err)
+	}
+	config = *c
+
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Printf("error creating Discord session: %s\n", err)
+		return
+	}
+	defer dg.Close()
+
+	dg.Identify.Intents = discordgo.IntentsGuildMembers
+
+	dg.AddHandler(guildMemberAdd)
+	dg.AddHandler(guildMemberUpdate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Printf("error opening connection: %s\n", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
+
+func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
+	if m.User.Bot {
+		botMustHaveBotRole(s, m.Member)
+	}
+}
+
+func guildMemberUpdate(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
+	if m.User.Bot {
+		botMustHaveBotRole(s, m.Member)
+	}
+}
+
+func readConfig(filename string) (*configData, error) {
+	buff, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &configData{}
+	err = yaml.Unmarshal(buff, c)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal in %q: %v", filename, err)
+	}
+
+	return c, nil
+}
+
+func memberNickname(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
+	if !config.Nickname.Enabled {
+		return
+	}
+
+	// Update member nickname to match prefix from config
+	for _, member := range config.Nickname.Members {
+		if member.ID == m.User.ID {
+			match := false
+			for _, prefix := range member.Prefix {
+				if m.Nick == prefix {
+					match = true
+					break
+				}
+			}
+
+			if !match {
+				s.GuildMemberNickname(m.GuildID, m.User.ID, member.Prefix[0])
+			}
+		}
+	}
+}
+
+func botMustHaveBotRole(s *discordgo.Session, m *discordgo.Member) {
+	if !config.BotRole.Enabled {
+		return
+	}
+
+	if !m.User.Bot {
+		return
+	}
+
+	for _, role := range config.BotRole.Roles {
+		if !contains(m.Roles, role) {
+			err := s.GuildMemberRoleAdd(m.GuildID, m.User.ID, role)
+			if err != nil {
+				fmt.Printf("error adding role %s to member %s: %s\n", role, m.User.ID, err)
+				continue
+			}
+
+			fmt.Printf("`%s` was given the `%s` role\n", m.User.Username, role)
+		}
+	}
+}
+
+// NOTE: This function create by Github copilot
+// Create contains function that pass string array and string to check if string is in array
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
